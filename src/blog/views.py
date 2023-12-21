fd9c1d2a3d7a7d377ff722b4a1f44de0fe86ccdf
Tomas Martinez <tomasagustinmartinez28@gmail.com>
2023-12-21 20:54:04 -0300

    mostrando posteos con mas likes

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-
+from publicaciones.models import Publicacion
 
 # vista basada en una funcion
 def index_view(request):
-    return render(request, 'index.html', {})
+    ctx = {
+        'mejores_posteo' : Publicacion.objects.order_by('-me_gusta')[:1]
+    }
+    return render(request, 'index.html', ctx)
 
